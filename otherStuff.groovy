fef535bfdb28fa9e7a0d8e16a0198ae2c393d70e
Joseph Adams <joseph.adams@blueyonder.co.uk>
2020-12-19 23:11:09 +0000

    blah8

---
+++ otherStuff.groovy
@@ -0,0 +1,15 @@
+def build() {
+
+    stage('Build') {
+        echo 'in the build shit'
+    }
+}
+def results() {
+
+    stage('Results') {
+        echo 'in the results shit'
+    }
+
+}
+
+return this;
